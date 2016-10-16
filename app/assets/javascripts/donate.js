b4a9c2e88dc5c98c6c177b5d2d9eacbc0f4bb4cb
John Rees <john@bitsushi.com>
2016-10-16 23:43:33 +0100

    donation stuff

---
+++ app/assets/javascripts/donate.js
@@ -0,0 +1,12 @@
+document.addEventListener("turbolinks:load", function() {
+  $('label.amount').each(function(e) {
+    var radio = $(this).find('input[type=radio]').first()
+    console.log(radio)
+    if (radio.checked) {
+      $(this).addClass('selected')
+    } else {
+      $(this).removeClass('selected')
+    }
+    // radio.hide()
+  })
+})

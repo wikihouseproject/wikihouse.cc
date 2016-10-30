ed5b03dccba27a89c290d0ee29935c36d3f73c8b
John Rees <john@bitsushi.com>
2016-10-30 15:08:56 +0000

    slider work

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,7 +1,7 @@
 // document.addEventListener("ready", function() {
 $(document).ready(function() {
   if ( $('#slider').length > 0) {
-    $('#slider').delay(100).fadeIn()
+    $('#slider li').fadeIn('slow')
     $('#slider').unslider({
       arrows: false,
       animation: 'fade'

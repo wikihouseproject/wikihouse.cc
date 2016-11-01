e9f4a8431264775d5b874b8a47813ebeb2ab1640
John Rees <john@bitsushi.com>
2016-11-01 18:57:53 +0000

    tidying

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,7 +1,7 @@
 $(document).on("ready pjax:success", function() {
   if ($('.typed').length > 0) {
     $(".typed").typed({
-      strings: ["^1000The future of homes.^500<br/>By everyone,^500 for<br/>everyone.", "^1000Simple,^300 sustainable,^300<br/> open building<br/> technologies."],
+      strings: ["^500The future of homes.^500<br/>By everyone,^500 for<br/>everyone.", "^1000Simple,^300 sustainable,^300<br/> open building<br/> technologies."],
       backDelay: 2000,
       startDelay: 1000,
       typeSpeed: 15,

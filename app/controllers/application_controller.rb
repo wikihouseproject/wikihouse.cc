8afee47680f57b6a0d748ef437b61664d74fb326
John Rees <john@bitsushi.com>
2016-11-09 11:53:41 +0000

    temp disable csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery# with: :exception
+  # protect_from_forgery# with: :exception
   http_basic_authenticate_with name: "poka", password: "yoke" if Rails.env.production?
 end

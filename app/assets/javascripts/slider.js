a6d1c44a3a57883a6a9dbae8d121fd5d4d1582b0
John Rees <john@bitsushi.com>
2016-12-15 23:06:26 +0000

    slider arrows

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,13 +1,20 @@
 // document.addEventListener("ready", function() {
 $(document).on("ready pjax:success", function() {
+
   if ( $('.slider').length > 0) {
     $('.slider li').fadeIn('slow')
     var slider = $('.slider').unslider({
-      // arrows: false,
+      arrows: false,
       animation: 'fade'
     })
     slider.on('unslider.change', function(event, index, slide) {
       $('.slider .hidden').removeClass('hidden')
+    })
+
+    $('a[data-slider-control]').click(function(e) {
+      e.preventDefault();
+      $(this).parents('.slider').unslider($(this).data('slider-control'))
+      $('a[data-slider-control]')
     })
   }
   

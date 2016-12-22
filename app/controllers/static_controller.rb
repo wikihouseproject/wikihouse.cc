07192fc52e5e88d635c26e0b259a4ea9ab6a71b8
John Rees <john@bitsushi.com>
2016-12-22 21:50:12 +0000

    landing

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -16,4 +16,15 @@ class StaticController < ApplicationController
     render 'communities_guide', layout: false
   end
 
+  def community
+    @img = "https://static.wikihouse.cc/MiniWiki-v1.1.jpg"
+    @title = "New year, new website."
+    @url = "https://medium.com/@WikiHouse/new-year-new-website-bcb1dd233bbe"
+    # "https://medium.com/@AlastairParvin/scaling-the-citizen-sector-20a20dbb7a4c"
+    # x.at_xpath('//item//title').text
+    # <%= x2.at_xpath('//img').attr('src') %>
+    # x = File.open("data/alastairparvin.xml") { |f| Nokogiri::XML(f) } %>
+    # x2 = Nokogiri::XML(x.at_xpath('//item//description')) %>
+  end
+
 end

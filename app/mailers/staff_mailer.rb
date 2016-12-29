72e4e941dc5bf6276250daf17de9d135f9cccfd8
John Rees <john@bitsushi.com>
2016-12-29 23:17:31 +0000

    mailer work

--- app/mailers/staff_mailer.rb
+++ app/mailers/staff_mailer.rb
@@ -8,9 +8,9 @@ class StaffMailer < ApplicationMailer
 
   def enquiry enquiry_id
     @enquiry = Enquiry.find(enquiry_id)
-    mail to: "admin@wikihouse.cc",
+    mail to: "admin+#{@enquiry.kind}@wikihouse.cc",
       reply_to: "#{@enquiry.first_name} #{@enquiry.last_name} <#{@enquiry.email}>",
-      subject: "[WikiHouse] #{@enquiry.kind.gsub('-',' ').humanize} enquiry",
-      from: "#{@enquiry.first_name} #{@enquiry.last_name} <no-reply@#{ENV.fetch('mailgun_domain')}>"
+      subject: "[WikiHouse] #{@enquiry.kind.gsub('-',' ').humanize}",
+      from: "#{@enquiry.first_name} #{@enquiry.last_name} <no-reply-#{1000+enquiry_id}@#{ENV.fetch('mailgun_domain')}>"
   end
 end

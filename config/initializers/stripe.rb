4ca194f377bb105d070004b22329b5ea98e1b496
John Rees <john@bitsushi.com>
2017-02-15 16:06:22 +0000

    removed stripe initializer

--- config/initializers/stripe.rb
+++
@@ -1,6 +0,0 @@
-Rails.configuration.stripe = {
-  :publishable_key => ENV.fetch('stripe_public_key'),
-  :secret_key      => ENV.fetch('stripe_secret_key')
-}
-
-Stripe.api_key = Rails.configuration.stripe[:secret_key]

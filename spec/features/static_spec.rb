1daf4dffd5246670ff474d3493bf65759b0840fe
Jon Leighton <j@jonathanleighton.com>
2017-02-23 16:17:18 +0000

    Refactor test
    I think this structure is easier to understand

--- spec/features/static_spec.rb
+++ spec/features/static_spec.rb
@@ -2,26 +2,27 @@ require 'rails_helper'
 
 describe "static" do
 
-  it "has a homepage" do
-    visit "/"
-    expect(page).to have_title "WikiHouse"
-  end
-
-  [ 'About', 'Library', 'R&D', 'Partners',
-    'Propose a pilot', 'About WikiHouse Foundation',
-    ['Contact Us', 'Becoming a partner'], 'Jobs', 'Donate'].each do |link|
-
-    if link.kind_of?(Array)
-      link = link[0]
-      title = link[1]
-    end
-
-    it "has a '#{link}' page" do
+  def self.has_page(link_name, title: link_name)
+    it "has a '#{link_name}' page" do
       visit "/"
-      click_link(link, match: :first)
-      expect(page).to have_title(title||link)
+      click_link(link_name, match: :first)
+      expect(page).to have_title(title)
     end
-
   end
+
+  it "has a home page" do
+    visit "/"
+    expect(page).to have_title("WikiHouse")
+  end
+
+  has_page "About"
+  has_page "Library"
+  has_page "R&D"
+  has_page "Partners"
+  has_page "Propose a pilot"
+  has_page "About WikiHouse Foundation"
+  has_page "Contact Us", title: "Becoming a partner"
+  has_page "Jobs"
+  has_page "Donate"
 
 end

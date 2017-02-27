d56f57b305c387aa915e02cd93ce37a15fbd1fdf
Jon Leighton <j@jonathanleighton.com>
2017-02-27 11:58:19 +0000

    Render public/404.html when PushType node is not found
    PushType adds a catch all route, and then tries to find the permalink in
    the database. If it doesn't find the permalink, it raises
    ActiveRecord::RecordNotFound. Therefore, we need to handle this
    exception in order to render a 404 page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  # PushType raises this when a node's permalink isn't in the database
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
   def cms_content?
     params[:cms]
   end
   helper_method :cms_content?
+
+  def render_not_found
+    render text: Rails.root.join("public/404.html").read
+  end
 end

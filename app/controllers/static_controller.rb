ae424add03a2650065bc938b900f204b55b55953
Jon Leighton <j@jonathanleighton.com>
2017-03-08 13:15:00 +0000

    Remove obsolete action

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -6,13 +6,4 @@ class StaticController < ApplicationController
     @about_points = AboutPoint.order(ordinal: :asc)
   end
 
-  def community
-    @medium_post = OpenStruct.new({
-      img: "https://static.wikihouse.cc/MiniWiki-v1.1.jpg",
-      title: "New year, new website.",
-      url: "https://medium.com/@WikiHouse/new-year-new-website-bcb1dd233bbe",
-      background_css: "background-color: #1C2229; background-size: cover; background-position: 83% center;"
-    })
-  end
-
 end

7e88fd02075ef559972e683019269eaf46be4d40
Jon Leighton <j@jonathanleighton.com>
2017-04-05 16:06:25 +0100

    Add PartnersPage#pricing_table field
    We'll just put the raw HTML in this
    
    https://trello.com/c/bRCfsje3/154-add-the-pricing-diagram-to-cms

--- app/models/partners_page.rb
+++ app/models/partners_page.rb
@@ -1,6 +1,8 @@
 class PartnersPage < PushType::Node
   has_child_nodes :partner, :tile
 
+  field :pricing_table, :markdown, validates: { presence: true }
+
   def tiles
     children.published.where(type: "Tile")
   end
